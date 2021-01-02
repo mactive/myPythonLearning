9bcecd1ba325c97f1676d89c3aae6250e5fbd2f4
mactive <mactive@gmail.com>
2021-01-02 09:25:09 +0800

    16 读写文件

---
+++ prac_16.py
@@ -0,0 +1,31 @@
+from sys import argv
+script, filename = argv
+
+print(f"We're going to erase {filename}.")
+
+print("Opening the file...")
+target = open(filename, "w")
+
+print("Truncating the file. Goodbyg!")
+target.truncate()
+
+print("Now I'm going to ask you for three lines.")
+line1 = input("Line1:")
+line2 = input("Line2:")
+line3 = input("Line3:")
+
+print("I'm going to write these to the file")
+
+target.write(line1)
+target.write("\n")
+target.write(line2)
+target.write("\n")
+target.write(line3)
+target.write("\n")
+
+target.close()
+
+print("And finally, we close it.")
+
+finalfile = open(filename)
+print(finalfile.read())
